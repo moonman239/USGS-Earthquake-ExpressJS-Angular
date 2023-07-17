f763b9d2dea445474873f7550028ad9ca9c47786
Montana Burr <bur18019@byui.edu>
2023-07-17 14:05:34 -0600

    added error handling to /near

--- backend/index.ts
+++ backend/index.ts
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 var earthquakes = require("./earthquakes");
 
-app.get('/', async (req, res) => {
+app.get('/', async (req, res,next) => {
   const startDate = new Date(req.query.start_date);
   const endDate = new Date(req.query.end_date);
   res.send(JSON.stringify(await Earthquake.getEarthquakes(startDate,endDate)));
@@ -13,17 +13,22 @@ app.get('/', async (req, res) => {
 
 
 
-app.get('/near',async(req,res)=>{
+app.get('/near',async(req,res,next)=>{
   const startDate = new Date(req.query.start_date ? req.query.start_date : "1776-07-04");
   const endDate = new Date(req.query.end_date);
   const searchRadius = parseInt(req.query.search_radius); // miles
   // get haversine distance from location coords to earthquake
   const latitude = parseFloat(req.query.latitude);
   const longitude = parseFloat(req.query.longitude);
-  const earthquakes = await Earthquake.getEarthquakes(startDate,endDate);
-
-  const nearbyEarthquakes = earthquakes.filter((earthquake: Earthquake)=>earthquake.distance(latitude,longitude) <= searchRadius);
-  res.send(JSON.stringify(nearbyEarthquakes));
+  try {
+    const earthquakes = await Earthquake.getEarthquakes(startDate,endDate);
+    const nearbyEarthquakes = Earthquake.withinRadius(earthquakes,latitude,longitude,searchRadius);
+    res.send(JSON.stringify(nearbyEarthquakes));
+  }
+  catch (e)
+  {
+    next(e); // pass errors to Express
+  }
 })
 
 
