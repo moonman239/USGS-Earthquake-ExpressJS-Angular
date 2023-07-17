3eee6a48b7b72bf5ff844c636761e5aa84671675
Montana Burr <bur18019@byui.edu>
2023-07-17 11:24:34 -0600

    fixed 'fetch' error and transitioned to Earthquake class'

--- backend/earthquakes.ts
+++ backend/earthquakes.ts
@@ -1,19 +1,39 @@
-type Earthquake = {
-    place: string;
-    time: number;
-    updated: string;
-    url: string;
-    detail:string};
-const fetch = require("node-fetch-commonjs");
-export async function getEarthquakes(startDate: Date,endDate: Date)
+import fetch from "node-fetch-commonjs";
+type Geometry = {coordinates: number[]};
+function radians(degrees: number)
 {
-    const url = new URL("https://earthquake.usgs.gov/fdsnws/event/1/query")
-    url.searchParams.append("format","geojson");
-    url.searchParams.append("starttime",startDate.toISOString());
-    url.searchParams.append("endtime",endDate.toISOString());
-    const response = await fetch(url);
-    if (!response.ok)
-        throw new Error("Error " + response.status + ": " + response.statusText);
-    const json = await response.json();
-    return json["features"];
+  return degrees * Math.PI / 180;
+}
+
+class Feature
+{
+    properties:
+        {place: string, time: number, geometry: Geometry}
+};
+
+export class Earthquake {
+    place:string
+    time:number
+    geometry:Geometry
+    constructor(place: string, time: number,geometry:Geometry)
+    {
+        this.place = place;
+        this.time = time;
+        this.geometry = geometry;
+    }
+    public static async getEarthquakes(startDate: Date,endDate: Date)
+    {
+        const url = new URL("https://earthquake.usgs.gov/fdsnws/event/1/query")
+        url.searchParams.append("format","geojson");
+        url.searchParams.append("starttime",startDate.toISOString());
+        url.searchParams.append("endtime",endDate.toISOString());
+        const response = await fetch(url);
+        if (!response.ok)
+            throw new Error("Error " + response.status + ": " + response.statusText);
+        const json = await response.json() as {features: Feature[]};
+        if (!json.features)
+            throw new Error("invalid json ");
+        return json.features.map((obj)=>new Earthquake(obj.properties.place,obj.properties.time,obj.properties.geometry));
+        
+    }
 }
